58f46db756be67696bea0870f051c949262e8e3e
KavindiUU <it22269416@my.sliit.lk>
2026-08-22 09:45:28 +0530

    update

--- src/main/java/lk/travelmarket/search_engine/dao/Blackouts.java
+++ src/main/java/lk/travelmarket/search_engine/dao/Blackouts.java
@@ -1,4 +1,36 @@
 package lk.travelmarket.search_engine.dao;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Blackouts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "hotel_id", nullable = false)
+    private Long hotelId;
+
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(name = "reason")
+    private String reason;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
 }
