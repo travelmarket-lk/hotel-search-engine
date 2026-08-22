e19af85ca93e21ca2dcd3610e9a72cb479017a8a
Prashan Silva <prashansilva@gmail.com>
2026-08-22 10:52:27 +0530

    Fix BedType Issue

--- src/main/java/lk/travelmarket/search_engine/dao/HotelRoom/Room.java
+++ src/main/java/lk/travelmarket/search_engine/dao/HotelRoom/Room.java
@@ -1,7 +1,12 @@
 package lk.travelmarket.search_engine.dao.HotelRoom;
 
 import jakarta.persistence.*;
+import lk.travelmarket.search_engine.dao.Contact;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -32,14 +37,15 @@ public class Room {
     @Column(name = "max_pax_count", nullable = false)
     private Integer maxPaxCount;
 
-//    @Column(name = "room_type_id")
-//    private Long roomTypeId;
-
     @Column(name = "hotel_id", nullable = false)
     private Long hotelId;
 
-    @OneToMany
-    @JoinColumn(name = "bed_type_id")
-    private BedType bed_type;
+    @ManyToMany
+    @JoinTable(
+            name = "room_bed_type",
+            joinColumns = @JoinColumn(name = "room_id"),
+            inverseJoinColumns = @JoinColumn(name = "bed_type_id")
+    )
+    private Set<BedType> bedTypes = new HashSet<>();
 
 }
