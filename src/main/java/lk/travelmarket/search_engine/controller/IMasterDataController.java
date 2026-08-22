495028995df69453786ec129bad60e179f3a6d9b
Prashan Silva <prashansilva@gmail.com>
2026-08-22 10:02:52 +0530

    Master Data Controller Adding

---
+++ src/main/java/lk/travelmarket/search_engine/controller/IMasterDataController.java
@@ -0,0 +1,10 @@
+package lk.travelmarket.search_engine.controller;
+
+import lk.travelmarket.search_engine.util.EndpointConstants;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(EndpointConstants.V1)
+public interface IMasterDataController {
+}
---
+++ src/main/java/lk/travelmarket/search_engine/controller/MasterDataController.java
@@ -0,0 +1,7 @@
+package lk.travelmarket.search_engine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MasterDataController implements IMasterDataController{
+}
