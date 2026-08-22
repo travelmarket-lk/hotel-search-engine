1b17a5765e6f85de96adf3990806b09ddbd23ee6
හිමෙත් සදෙව් හේවාවිතාරණ <sandevhewavitharana@gmail.com>
2026-08-22 16:02:14 +0530

    PolicyService

---
+++ src/main/java/lk/travelmarket/search_engine/service/Policy/PolicyService.java
@@ -0,0 +1,27 @@
+package lk.travelmarket.search_engine.service.Policy;
+
+import lk.travelmarket.search_engine.Repository.PolicyRepository;
+import lk.travelmarket.search_engine.dao.Policy.Policy;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PolicyService {
+
+    private final PolicyRepository policyRepository;
+
+    public PolicyService(PolicyRepository policyRepository) {
+        this.policyRepository = policyRepository;
+    }
+
+    public List<Policy> getAllPolicies() {
+        return getAllPolicies();
+    }
+
+    public Policy savePolicy(Policy policyRepository) {
+        return savePolicy(policyRepository);
+    }
+
+}
