24af10d9a2ccbde0d5a58da22618fcea43c29a65
Poojani Hirunima <poojanihirunima457@gmail.com>
2026-09-05 09:14:30 +0530

    fix Constants 2

--- src/main/java/lk/travelmarket/search_engine/util/Constants.java
+++ src/main/java/lk/travelmarket/search_engine/util/Constants.java
@@ -2,17 +2,17 @@ package lk.travelmarket.search_engine.util;
 
 public class Constants {
 
-//    public static final String SUCCESS_RETRIEVE_TESTS = "Tests retrieve success";
-//    public static final String ERROR_RETRIEVE_TESTS = "Tests retrieve failed";
-//    public static final String SUCCESS_CREATE_TEST = "Test created successfully";
-//    public static final String ERROR_CREATE_TESTS = "Test creation failed";
-//    public static final String SUCCESS_RETRIEVE_TEST = "Test retrieve success";
-//    public static final String ERROR_RETRIEVE_TEST = "Test retrieve failed";
-//    public static final String ERROR_RETRIEVE_TEST_NOT_FOUND = "Test not found";
-//    public static final String SUCCESS_UPDATE_TEST = "Test updated successfully";
-//    public static final String ERROR_UPDATE_TEST = "Test update failed";
-//    public static final String SUCCESS_DELETE_TEST = "Test deleted successfully";
-//    public static final String ERROR_DELETE_TEST = "Test deletion failed";
+    public static final String SUCCESS_RETRIEVE_TESTS = "Tests retrieve success";
+    public static final String ERROR_RETRIEVE_TESTS = "Tests retrieve failed";
+    public static final String SUCCESS_CREATE_TEST = "Test created successfully";
+    public static final String ERROR_CREATE_TESTS = "Test creation failed";
+    public static final String SUCCESS_RETRIEVE_TEST = "Test retrieve success";
+    public static final String ERROR_RETRIEVE_TEST = "Test retrieve failed";
+    public static final String ERROR_RETRIEVE_TEST_NOT_FOUND = "Test not found";
+    public static final String SUCCESS_UPDATE_TEST = "Test updated successfully";
+    public static final String ERROR_UPDATE_TEST = "Test update failed";
+    public static final String SUCCESS_DELETE_TEST = "Test deleted successfully";
+    public static final String ERROR_DELETE_TEST = "Test deletion failed";
 
     // DISTRICT
 
