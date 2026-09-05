08aeb9ac5f381ad3040b00395b8854f1627da74c
හිමෙත් සදෙව් හේවාවිතාරණ <sandevhewavitharana@gmail.com>
2026-09-05 10:26:34 +0530

    Delete src/main/java/lk/travelmarket/search_engine/dao/test directory

--- src/main/java/lk/travelmarket/search_engine/dao/test/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package lk.travelmarket.search_engine.dao.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
