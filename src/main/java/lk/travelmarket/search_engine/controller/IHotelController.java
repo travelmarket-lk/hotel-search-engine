9844bb0b13ac272250f7513a2662216eded59fcb
Randidu <randidudamsith96@gmail.com>
2026-09-20 08:59:53 +0530

    Implement hotel validation

--- src/main/java/lk/travelmarket/search_engine/controller/IHotelController.java
+++ src/main/java/lk/travelmarket/search_engine/controller/IHotelController.java
@@ -1,6 +1,6 @@
 package lk.travelmarket.search_engine.controller;
 
-import lk.travelmarket.search_engine.dao.hotel.Hotel;
+import jakarta.validation.Valid;
 import lk.travelmarket.search_engine.dto.HotelDto;
 import lk.travelmarket.search_engine.dto.criteria.HotelCreationCriteria;
 import lk.travelmarket.search_engine.network.CCResponseWrapper;
@@ -16,14 +16,19 @@ public interface IHotelController {
     ResponseEntity<CCResponseWrapper<HotelDto>> getAllHotels();
 
     @GetMapping(EndpointConstants.HOTEL_BY_ID)
-    ResponseEntity<CCResponseWrapper<HotelDto>> getHotelById(@PathVariable Long id);
+    ResponseEntity<CCResponseWrapper<HotelDto>> getHotelById(
+            @PathVariable Long id);
 
     @PostMapping(EndpointConstants.HOTEL)
-    ResponseEntity<CCResponseWrapper<HotelDto>> createHotel(@RequestBody HotelCreationCriteria criteria);
+    ResponseEntity<CCResponseWrapper<HotelDto>> createHotel(
+            @Valid @RequestBody HotelCreationCriteria criteria);
 
     @PutMapping(EndpointConstants.HOTEL_BY_ID)
-    ResponseEntity<CCResponseWrapper<HotelDto>> updateHotel(@PathVariable Long id, @RequestBody HotelDto hotel);
+    ResponseEntity<CCResponseWrapper<HotelDto>> updateHotel(
+            @PathVariable Long id,
+            @Valid @RequestBody HotelDto hotel);
 
     @DeleteMapping(EndpointConstants.HOTEL_BY_ID)
-    ResponseEntity<CCResponseWrapper<Boolean>> deleteHotel(@PathVariable Long id);
+    ResponseEntity<CCResponseWrapper<Boolean>> deleteHotel(
+            @PathVariable Long id);
 }
